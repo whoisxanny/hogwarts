b5d21ce1761118b1047bf60dd064685b7a74c211
whoisxanny <tuffx4nny@gmail.com>
2023-11-19 22:13:21 +0500

    All donee

--- src/Main.java
+++ src/Main.java
@@ -2,45 +2,6 @@ import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-//        Hogwarts[] hogwarts = {
-////                new Hogwarts("Поттер", "Гарри", "Гриффиндор", 10, 13),
-////                new Hogwarts("Грейнджер ", "Гермиона", "Гриффиндор", 12, 13),
-////                new Hogwarts("Уизли", "Рон", "Гриффиндор", 8, 8),
-////                new Hogwarts("Драко", "Малфой", "Слизерин", 13, 11),
-////                new Hogwarts("Грэхэм", "Монтегю", "Слизерин", 9, 8),
-////                new Hogwarts("Гойл", "Грегори", "Слизерин", 7, 6),
-////                new Hogwarts("Смит", "Захария", "Пуффендуй", 6, 6),
-////                new Hogwarts("Диггори", "Седрик", "Пуффендуй", 7, 7),
-////                new Hogwarts("Финч", "Джастин", "Пуффендуй", 7, 6),
-////                new Hogwarts("Чанг", "Чжоу", "Когтевран", 9, 8),
-////                new Hogwarts("Патил", "Падма", "Когтевран", 10, 9),
-////                new Hogwarts("Белби", "Маркус", "Когтевран", 11, 11),
-//                id0003,
-//        };
-//
-//                Gryffindor id0001 = new Gryffindor("Поттер", "Гарри", "Гриффиндор", 10, 13, 5, 6, 7),
-//                Gryffindor id0002 = new Gryffindor("Грейнджер ", "Гермиона", "Гриффиндор", 12, 13, 6, 7, 8),
-//                Gryffindor id0003 = new Gryffindor("Уизли", "Рон", "Гриффиндор", 8, 8, 5, 5, 5),
-//
-//                Slytherin id0004 = new Slytherin("Драко", "Малфой", "Слизерин", 13, 11, 8, 9, 10),
-//                Slytherin id0005 = new Slytherin("Грэхэм", "Монтегю", "Слизерин", 9, 8, 6, 7, 7),
-//                Slytherin id0006 = new Slytherin("Гойл", "Грегори", "Слизерин", 7, 6, 5, 5, 5)
-//
-//                Ravenclaw[] ravenclaws = {
-//                        new Ravenclaw("Чанг", "Чжоу", "Когтевран", 9, 8, 7, 7, 6),
-//                        new Ravenclaw("Патил", "Падма", "Когтевран", 10, 9, 4, 5, 6),
-//                        new Ravenclaw("Белби", "Маркус", "Когтевран", 11, 11, 5, 6, 7)
-//                };
-//
-//                Hufflepuff[] hufflepuffs = {
-//                        new Hufflepuff("Смит", "Захария", "Пуффендуй", 6, 6, 4, 4, 4),
-//                        new Hufflepuff("Диггори", "Седрик", "Пуффендуй", 7, 7, 3, 2, 3),
-//                        new Hufflepuff("Финч", "Джастин", "Пуффендуй", 7, 6, 3, 5, 5)
-//                };
-
-
-//        System.out.println("hogwarts.length = " + hogwarts.length);
-//        describeStudent(hogwarts[6]);
 
         Gryffindor harry = new Gryffindor("Поттер", "Гарри", "Гриффиндор", 10, 13, 5, 6, 7);
         Gryffindor hermione = new Gryffindor("Грейнджер ", "Гермиона", "Гриффиндор", 12, 13, 6, 7, 8);
